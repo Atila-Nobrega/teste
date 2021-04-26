27daa0166d21e30486f0f06b131b0da0571414fc
Atila-Nobrega <atila.aires2@gmail.com>
2021-04-26 17:35:21 -0300

    Update const.py

--- utils/const.py
+++ utils/const.py
@@ -1,6 +1,7 @@
 DB_HOST = "localhost"
 DB_NAME = "UFCSC"
-DB_USER = "postgres"
+DB_USER = "admin"
 DB_PASSWORD = "admin"
+DB_PORT = "54320"
 
-DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
